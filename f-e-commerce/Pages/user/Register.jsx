92372b6a75fdf9c765b5c6bdcf7bc73ea4e5622c
rahulvir <rahulmsd99999@gmail.com>
2024-08-24 03:07:14 +0530

    fix register page

--- f-e-commerce/Pages/user/Register.jsx
+++ f-e-commerce/Pages/user/Register.jsx
@@ -56,7 +56,7 @@ const Register = () => {
     }
   };
   return (
-    <section className=" h-[112vh] bg-gray-50 dark:bg-gray-900 box-border ">
+    <section className=" min-h-[100vh] bg-gray-50 dark:bg-gray-900 box-border ">
       <div className=" flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-full lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6  space-y-4 md:space-y-6 sm:p-8">
